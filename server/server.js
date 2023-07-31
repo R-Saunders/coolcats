2e15f18044c6023ac5e890e5bba2e85ee90be725
R-Saunders <richsaunders@protonmail.com>
2023-07-31 10:57:45 +0100

    add basic server config

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bp = require("body-parser");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8087;
+
+const app = express();
+app.use(cors());
+app.use(bp.json());
+
+app.get("/", (request, response) => {
+	response.status(200).json("Hello World");
+});
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
